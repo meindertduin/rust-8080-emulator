2514eb3a490fe870ddcb8bfe5c29a2233e80286c
meindertduin <meindertduin@hotmail.com>
2021-02-21 20:48:30 +0000

    added instructions untill 0x3a

--- src/cpu.rs
+++ src/cpu.rs
@@ -133,6 +133,13 @@ impl State8080 {
         }
     }
 
+    fn m(&self) -> u8 {
+        self.memory[self.hl.both() as usize]
+    }
+
+    fn m_mut(&mut self) -> &mut u8 {
+        &mut self.memory[self.hl.both() as usize]
+    }
 
     fn read_byte(&self, address: u16) -> u8 {
         self.memory[address as usize]
@@ -173,6 +180,7 @@ impl State8080 {
         self.flags.set_all_but_carry(result);
         result
     }
+
 
     fn cma(&mut self) {
         self.a = ! self.a;
@@ -319,41 +327,275 @@ impl State8080 {
     }
     
 
-    pub fn emulate(&mut self, ) {
+    pub fn emulate(&mut self) {
         let opcode = self.memory[self.pc as usize];
 
-        let op_size = match opcode {
-            0x00 | 0x20 => 1,
+        let (op_size, cycles) = match opcode {
+            // NOP
+            0x00 | 0x20 => (1, 4),
+            // LXB, D16
             0x01 => {
                 *self.bc.both_mut() = self.read_next_instruction_bytes();
-                3
+                (3, 10)
             },
+            // STAX B
             0x02 => {
                 self.write_byte(self.bc.both(), self.a);
-                1
+                (1, 7)
             },
+            // INX B
             0x03 => {
                 *self.bc.both_mut() = self.bc.both().wrapping_add(1);
-                1 
+                (1, 5)
             },
+            // INR B
             0x04 => {
                 *self.bc.msb_mut() = self.inr(self.bc.msb());
-                1 
+                (1, 5) 
             },
+            // DCR B
             0x05 => {
                 *self.bc.msb_mut() = self.dec(self.bc.msb());
-                1
+                (1, 5)
             }
+            // MVI B, D8
             0x06 => {
                 *self.bc.msb_mut() = self.read_next_instruction_byte();
-                2
+                (2, 7)
             },
+            // RLC
             0x07 => {
                 let prev_bit7: u8 = self.a & (1 << 7);
                 self.a <<= 1;
                 self.flags.carry = prev_bit7 != 0;
-                1
+                (1, 4)
             },
+            // DAD B
+            0x09 => {
+                self.dad(self.bc.both());
+                (1, 10)
+            },
+            // LDAX B
+            0x0a => {
+                self.a = self.read_byte(self.bc.both());
+                (1, 7)
+            },
+            // DCX B
+            0x0b => {
+               *self.bc.both_mut() = self.bc.both().wrapping_sub(1);
+               (1, 5)
+            },
+            // INR C
+            0x0c => {
+                *self.bc.msb_mut() = self.inr(self.bc.msb());
+                (1 ,5)
+            },
+            // DCR C
+            0x0d => {
+                *self.bc.msb_mut() = self.dec(self.bc.msb());
+                (1, 5)
+            },
+            // MVI C,D8
+            0x0e => {
+                *self.bc.lsb_mut() = self.read_next_instruction_byte();
+                (2, 7)
+            },
+            // RRC
+            0x0f => {
+                let bit0: u8 = self.a & 1;
+                self.a >>= 1;
+                self.a |= bit0 << 7;
+                self.flags.carry = bit0 != 0;
+                (1 ,4)
+            },
+            // LXI D, D16
+            0x11 => {
+               *self.de.both_mut() = self.read_next_instruction_bytes();
+               (3, 10)
+            },
+            // STAX D
+            0x12 => {
+                self.write_byte(self.de.both(), self.a);
+                (1, 7)
+            },
+            // INX D
+            0x13 => {
+                *self.de.both_mut() = self.de.both().wrapping_add(1);
+                (1, 5)
+            },
+            // INR D
+            0x14 => {
+               *self.de.msb_mut() = self.inr(self.de.msb());
+               (1, 5)
+            },
+            // DCR D
+            0x15 => {
+                *self.de.msb_mut() = self.dec(self.de.msb());
+                (1, 5)
+            },
+
+            // MVI D, D8
+            0x16 => {
+                *self.de.msb_mut() = self.read_next_instruction_byte();
+                (2, 7)
+            },
+
+            // RAL
+            0x17 => {
+                let bit7: u8 = self.a & (1 << 7);
+                self.a <<= 1;
+                self.a |= self.flags.carry as u8;
+                self.flags.carry = bit7 != 0;
+                (1, 4)
+            },
+            // DAD D
+            0x19 => {
+                self.dad(self.de.both());
+                (1, 10)
+            },
+            // LDAX D
+            0x1a => {
+               self.a =  self.read_byte(self.de.both());
+               (1, 7)
+            },
+            // DCX D
+            0x1b => {
+                *self.de.both_mut() = self.de.both().wrapping_sub(1);
+                (1, 5)
+            },
+            // INR E
+            0x1c => {
+                *self.de.lsb_mut() = self.inr(self.de.lsb());
+                (1, 5)
+            },
+            // DCR E
+            0x1d => {
+                *self.de.lsb_mut() = self.dec(self.de.lsb());
+                (1, 5)
+            },
+            // MVI E, D8
+            0x1e => {
+                *self.de.lsb_mut() = self.read_next_instruction_byte();
+                (2, 7)
+            },
+            // RAR
+            0x1f => {
+                let bit0: u8 = self.a * 1;
+                self.a >>= 1;
+                self.a |= bit0 << 7;
+                self.flags.carry = bit0 != 0;
+                (1, 4)
+            },
+            // LXI H, D16
+            0x21 => {
+               *self.hl.both_mut() = self.read_next_instruction_bytes();
+               (3, 10)
+            },
+            // SHLD adr
+            0x22 => {
+                self.write_bytes(self.read_next_instruction_bytes(), self.hl.both());
+                (3, 16)
+            },
+            // INX H
+            0x23 => {
+                *self.hl.both_mut() = self.hl.both().wrapping_add(1);
+                (1, 5)
+            },
+            // INR H
+            0x24 => {
+                *self.hl.msb_mut() = self.inr(self.hl.msb());
+                (1, 5)
+            },
+            // DCR H
+            0x25 => {
+                *self.hl.msb_mut() = self.dec(self.hl.msb());
+                (1, 5)
+            },
+            // MVI H, D8
+            0x26 => {
+                *self.hl.msb_mut() = self.read_next_instruction_byte();
+                (2, 7)
+            },
+            // DAA
+            0x27 => {
+                self.daa();
+                (1, 4)
+            },
+            // DAD H
+            0x29 => {
+                self.dad(self.hl.both());
+                (3, 10)
+            },
+            // LHLD adr
+            0x2a => {
+                *self.hl.both_mut() = self.read_bytes(self.read_next_instruction_bytes());
+                (3, 10)
+            },
+            // DCX H
+            0x2b => {
+                *self.hl.both_mut() = self.hl.both().wrapping_sub(1);
+                (1, 5)
+            },
+            // INR L
+            0x2c => {
+                *self.hl.lsb_mut() = self.inr(self.hl.lsb());
+                (1, 5)
+            },
+            // DCR L
+            0x2d => {
+                *self.hl.lsb_mut() = self.dec(self.hl.lsb());
+                (1, 5)
+            },
+            0x2e => {
+                *self.hl.lsb_mut() = self.read_next_instruction_byte();
+                (2, 7)
+            },
+            // CMA
+            0x2f => {
+                self.a = !self.a;
+                (1, 4)
+            },
+            // LXI SP, D16
+            0x31 => {
+                self.sp = self.read_next_instruction_bytes();
+                (3, 10)
+            },
+            // STA adr
+            0x32 => {
+                self.write_byte(self.read_next_instruction_bytes(), self.a);
+                (3, 13)
+            },
+            // INX SP
+            0x33 => {
+                self.sp = self.sp.wrapping_add(1);
+                (1, 5)
+            },
+            // INR M
+            0x34 => {
+                *self.m_mut() = self.inr(self.m());
+                (1, 10)
+            }
+            // DCR M
+            0x35 => {
+                *self.m_mut() = self.dec(self.m());
+                (1, 10)
+            },
+            // MVI M, D8
+            0x36 => {
+                *self.m_mut() = self.read_next_instruction_byte();
+                (2, 10)
+            },
+            // STC
+            0x37 => {
+                self.flags.carry = true;
+                (1, 4)
+            },
+            // DAD SP
+            0x39 => {
+                self.dad(self.sp);
+                (3, 10)
+
+            }
             _ => panic!("unimplemented instruction {}", opcode),
         };
 
